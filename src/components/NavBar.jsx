740cf81e15a1d25d953079d575d430d3d2706960
Jacob Bananal <jacob.bananal@tek84.com>
2024-10-16 15:04:25 -0700

    NetflixClone - installed @fontsource/bebas-neue and added import "@fontsource/bebas-neue";  to main.jsx

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,7 +4,7 @@ import { CiSearch } from "react-icons/ci";
 const NavBar = () => {
   return (
     <div className='h-16 w-full flex flex-row items-center justify-between p-4 bg-black bg-opacity-80'>
-        <h1 className='text-red-500 text-3xl font-bold'>Netflix</h1>
+        <h1 className='text-red-500 text-4xl font-bebas'>Netflix</h1>
         <div className='flex flex-row items-center space-x-6 text-white mr-10'>
             <CiSearch size={24}/>
             <IoMdNotifications size={24}/>
