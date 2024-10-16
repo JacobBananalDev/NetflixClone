f7f3197ee76cde7cd911d108dbe91a4527030444
Jacob Bananal <jacob.bananal@tek84.com>
2024-10-16 15:15:22 -0700

    NetflixClone - Added NetflixIcon.ico and NetflixLandingPageBackground.jpg, Home.jsx, and LandingPage.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import { useState } from 'react';
-import NavBar from './components/NavBar';
+import LandingPage from './pages/LandingPage';
 
 function App() {
 
   return (
     <main className='flex'>
-      <NavBar/>
+      <LandingPage/>
     </main>
   );
 };
---
+++ src/pages/LandingPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import NetflixLandingPageBackground from '../assets/NetflixLandingPageBackground.jpg';
+
+const LandingPage = () => {
+  return (
+    <div className='min-h-screen w-full bg-black'>
+      <img src={NetflixLandingPageBackground}/>
+    </div>
+  );
+};
+
+export default LandingPage;
